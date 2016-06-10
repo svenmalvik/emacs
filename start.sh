d452bae64d1ec28e98f5c03acea622645e729d0a
Sven Malvik <sven@malvik.de>
2016-06-10 10:55:07 +0000

    hostname

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --name emacs -v $HOME:/home -v /var/run/docker.sock:/var/run/docker.sock svenmalvik/emacs $1
+docker run -it --rm --name emacs -e HOSTNAME=$HOSTNAME -v $HOME:/home -v /var/run/docker.sock:/var/run/docker.sock svenmalvik/emacs $1
