403502ff650b90b47a68a0d576ac7cb3093936c0
Sven Malvik <sven@malvik.de>
2016-06-18 19:34:57 +0000

    dockerfile

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
       display-time-day-and-date t)
       (display-time)
 
-(add-to-list 'load-path "/home/emacs/")
+(add-to-list 'load-path "/etc/emacs/")
 
 (require 'dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
